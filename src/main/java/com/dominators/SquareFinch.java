79f6a07099a0c7dd2ca2690695aaf76bd3dec8e0
Jsaka701 <jsakamoto0002@mymail.lausd.net>
2025-05-30 10:39:40 -0700

    sAVE

--- src/main/java/com/dominators/SquareFinch.java
+++ src/main/java/com/dominators/SquareFinch.java
@@ -8,8 +8,17 @@ public class SquareFinch{
 
         // DO SOMETHING
 
-        bird.setMove("F", 20, 100);
-
+        bird.setMove("F", 5, 200);
+        bird.setMove("B", 5, 200);
+        bird.setMove("F", 5, 200);
+        bird.setMove("B", 5, 200);
+        bird.setTurn("R", 360, 200);
+         bird.setTurn("L", 760, 200);
+        bird.setMove("F", 20,200);
+        bird.setTurn("R", 180, 200);
+        bird.setMove("F", 20,200);
+        bird.setMove("B", 50,200);
+bird.setMove("F", 50,200);
         bird.stop();
         bird.disconnect();
     }
